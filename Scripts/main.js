1b5ff2694cad70a585ff33d0746e281ad967e7f3
KimSon1391 <64429138+KimSon1391@users.noreply.github.com>
2021-07-08 00:22:50 +0700

    Message and Back To Top button

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,4 +1,5 @@
-﻿const searchBtn = document.querySelector('.search');
+﻿
+const searchBtn = document.querySelector('.search');
 const cartBtn = document.querySelector('.cart');
 const closeSearchBtn = document.querySelector('.navSearch .close');
 const closeCartBtn = document.querySelector('.navCart .close');
@@ -6,12 +7,15 @@ const overlay = document.querySelector('.overlay');
 const navSearch = document.querySelector('.navSearch');
 const navCart = document.querySelector('.navCart');
 
+
+//Close Nav
 function closeNav() {
     navSearch.classList.remove('active');
     navCart.classList.remove('active');
     overlay.classList.remove('active');
 }
 
+//Open Nav
 searchBtn.addEventListener('click', (e) => {
     e.stopPropagation();
     navSearch.classList.add('active');
@@ -25,6 +29,7 @@ cartBtn.addEventListener('click', (e) => {
 })
 
 
+//Close Nav Button
 closeSearchBtn.addEventListener('click', (e) => {
     e.stopPropagation();
     closeNav();
@@ -42,21 +47,27 @@ window.addEventListener('click', (e) => {
  
 })
 
+
+//Carousel Slider
 let slide_index = 0
 let slide_play = true
 let slides = document.querySelectorAll('.carousel__item')
 
+//Hide Carousel
 hideAllSlide = () => {
     slides.forEach(e => {
         e.classList.remove('active')
     })
 }
 
+//Show Carousel
 showSlide = () => {
     hideAllSlide()
     slides[slide_index].classList.add('active')
 }
 
+
+//Even Slider
 nextSlide = () => slide_index = slide_index + 1 === slides.length ? 0 : slide_index + 1
 
 prevSlide = () => slide_index = slide_index - 1 < 0 ? slides.length - 1 : slide_index - 1
@@ -79,3 +90,47 @@ document.querySelector('.left-arrow').addEventListener('click', () => {
 })
 
 showSlide()
+
+
+
+
+//Open-Close Message button form
+const openFormButton = document.querySelector('.open-button');
+const closeFormButton = document.querySelector('.chat-popup .btn.cancel');
+const myForm = document.getElementById("myForm")
+
+openFormButton.addEventListener('click', (e) => {
+    myForm.style.display = "block";
+})
+
+closeFormButton.addEventListener('click', () => {
+    myForm.style.display = "none";
+})
+
+
+
+
+//Get the button Back to Top
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+};
+
+
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+
+    
