db2ed1a1920406d20b9136db472dcc3008935734
chithanh0246 <nguyenchithanhwiko@gmail.com>
2021-07-24 17:34:45 +0700

    Đăng nhập Admin

---
+++ Areas/Admin/Models/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using LapTrinhWeb.Areas.Admin.Data;
+
+namespace LapTrinhWeb.Areas.Admin.Models
+{
+
+    public class Account
+    {
+        private QLBanHangDbContext db;
+        public Account()
+        {
+            db = new QLBanHangDbContext();
+        }
+        public bool Login(string user, string pass)
+        {
+            object[] sqlParams = new SqlParameter[]
+            {
+                new SqlParameter("@Email",user),
+                new SqlParameter("@Matkhau",pass)
+            };
+            var res = db.Database.SqlQuery<bool>("Sp_Account_Login @Email,@Matkhau", sqlParams).SingleOrDefault();
+            return res;
+        }
+    }
+}
---
+++ Areas/Admin/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LapTrinhWeb.Areas.Admin.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Email { get; set; }
+        public string Matkhau { get; set; }
+        public bool Remember { get; set; }
+    }
+}
