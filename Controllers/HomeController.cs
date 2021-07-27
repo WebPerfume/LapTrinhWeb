92140bdc843cbd97b26201f5e4eed2f38410d33c
chithanh0246 <nguyenchithanhwiko@gmail.com>
2021-07-27 15:35:09 +0700

    Sửa Giao diện xem chi tiết sản phẩm dùm đi Sơn ơi

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using LapTrinhWeb.Models;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,11 +12,32 @@ namespace LapTrinhWeb.Controllers
     public class HomeController : Controller
     {
         private QLBHEntities db = new QLBHEntities();
-        public ActionResult Index()
+        private List<Product> ListP()
         {
-            return View();
+            return db.Products.ToList();
+        }
+        public ActionResult Index(int? page)
+        {
+            if (page == null) page = 1;
+            var List = ListP();
+            int pagesize = 20;
+            int pagenumber = (page ?? 1);
+            return View(List.ToPagedList(pagenumber, pagesize));
         }
 
+        public ActionResult Preview(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
