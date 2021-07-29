2f363a94741df41b6025c28a7f893fcd3428e81f
WebPerfume <web18dthc5@gmail.com>
2021-07-29 18:02:35 +0700

    finish

--- Scripts/main.js
+++ Scripts/main.js
@@ -120,7 +120,63 @@ $(document).ready(function () {
         friction: 0.8,
     });
 
+    let headerTop = $('.header__top');
+    let headerBottom = $('.header__bottom');
+    $(window).scroll(function () {
+        let scrollY = $(window).scrollTop();
+        if (scrollY > headerBottom.outerHeight()) {
+        headerBottom.addClass("active");
+
+            
+        } else {
+         headerBottom.removeClass("active");
+        }
+    });
 
 
+    //GSAP
+    let textOvelay = $(".welcome_overlay");
+    let mainOverlay = $('.welcome');
+    let body = $('body');
+
+
+    tlClick = new gsap.timeline();
+    tlFirst = new gsap.timeline();
+
+    tlFirst.to(textOvelay, 2, { x: "100%", ease: Power1.easeInOut });
+    $(window).click(function () {
+        
+        tlFirst.timeScale(2).reverse();
+        tlClick.to(mainOverlay, 2, {
+ 
+            y: "-100%",
+            ease: Power4.easeInOut,
+            delay: 0.6,
+        });
+    });
+    gsap.set(".mouse", { xPercent: -50, yPercent: -50 });
+
+    const box = document.querySelector(".mouse");
+    const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+    const mouse = { x: pos.x, y: pos.y };
+    const speed = 0.35;
+
+    const xSet = gsap.quickSetter(box, "x", "px");
+    const ySet = gsap.quickSetter(box, "y", "px");
+
+    window.addEventListener("mousemove", (e) => {
+        mouse.x = e.x;
+        mouse.y = e.y;
+    });
+
+    gsap.ticker.add(() => {
+        // adjust speed for higher refresh monitors
+        const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio());
+
+        pos.x += (mouse.x - pos.x) * dt;
+        pos.y += (mouse.y - pos.y) * dt;
+        xSet(pos.x);
+        ySet(pos.y);
+    });
 })
 
